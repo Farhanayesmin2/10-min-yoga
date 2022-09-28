f26ffd3fdf87910e798f1b77085417e386dd1503
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-09-29 00:23:02 +0600

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Ten Minute Yoga Center.</h1>
+    <Header></Header>
     </div>
   );
 }
